add0195aa3174ac178f94b47d58d05bd4ada927f
halityurtsever <halityurtsever@gmail.com>
2019-10-28 20:26:06 +0100

    Currency formatter and additional constant value

--- HandyBoxApp/Utilities/Constants.cs
+++ HandyBoxApp/Utilities/Constants.cs
@@ -3,5 +3,7 @@
     internal static class Constants
     {
         public const int DefaultRefreshRate = 1000; //ms
+
+        public const string Precision = "{0:C4}";
     }
 }
---
+++ HandyBoxApp/Utilities/CurrencyFormater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandyBoxApp.Utilities
+{
+    internal class CurrencyFormater
+    {
+        internal static string Format(double amount, string cultureCode, string currencyCode)
+        {
+            CultureInfo cultureInfo = CultureInfo.CreateSpecificCulture(cultureCode);
+            cultureInfo.NumberFormat.CurrencySymbol = currencyCode;
+            cultureInfo.NumberFormat.CurrencyPositivePattern = 3;
+
+            return string.Format(cultureInfo, Constants.Precision, amount);
+        }
+    }
+}
