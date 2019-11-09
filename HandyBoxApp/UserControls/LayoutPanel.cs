fbb487e1c09b88bd00c3d925310b4261315c6818
halityurtsever <halityurtsever@gmail.com>
2019-11-09 12:34:02 +0100

    New cutom user controls created

---
+++ HandyBoxApp/UserControls/LayoutPanel.cs
@@ -0,0 +1,92 @@
+﻿using HandyBoxApp.CustomComponents;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HandyBoxApp.UserControls
+{
+    public class LayoutPanel : UserControl
+    {
+        //################################################################################
+        #region Fields
+
+        private FlowLayoutPanel containerPanel;
+
+        #endregion
+
+        //################################################################################
+        #region Constructor
+
+        public LayoutPanel()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        internal void Add(Control control)
+        {
+            containerPanel.Controls.Add(control);
+        }
+
+        //################################################################################
+        #region Private Members
+
+        private void InitializeComponent()
+        {
+            containerPanel = new FlowLayoutPanel();
+            SuspendLayout();
+
+            // 
+            // containerPanel
+            // 
+            containerPanel.Name = "containerPanel";
+            containerPanel.BackColor = Color.White;
+            containerPanel.BorderStyle = BorderStyle.FixedSingle;
+            containerPanel.FlowDirection = FlowDirection.TopDown;
+            containerPanel.Location = new Point(0, 0);
+            containerPanel.Padding = new Padding(Style.PanelSpacing, Style.PanelSpacing, Style.PanelSpacing, 0);
+            containerPanel.TabIndex = 0;
+            containerPanel.ControlAdded += ContainerPanel_ControlAdded;
+
+            // 
+            // LayoutPanel
+            // 
+            Name = "LayoutPanel";
+            AutoScaleMode = AutoScaleMode.Font;
+
+            Controls.Add(containerPanel);
+
+            ResumeLayout(false);
+        }
+
+        private void ContainerPanel_ControlAdded(object sender, ControlEventArgs e)
+        {
+            UpdateContainerPanel();
+        }
+
+        private void UpdateContainerPanel()
+        {
+            containerPanel.Width = 0;
+            containerPanel.Height = Style.PanelSpacing + Style.FormBorder;
+
+            foreach (Control control in containerPanel.Controls)
+            {
+                if (control.Width > containerPanel.Width)
+                {
+                    containerPanel.Width = control.Width;
+                }
+
+                containerPanel.Height += control.Height + Style.PanelSpacing;
+            }
+
+            containerPanel.Width += (Style.PanelSpacing + Style.FormBorder) * 2;
+            containerPanel.Height += Style.FormBorder;
+
+            Width = containerPanel.Width;
+            Height = containerPanel.Height;
+        }
+
+        #endregion
+    }
+}
---
+++ HandyBoxApp/UserControls/TitlePanel.cs
@@ -0,0 +1,133 @@
+﻿using HandyBoxApp.ColorScheme;
+using HandyBoxApp.ColorScheme.Colors;
+using HandyBoxApp.CustomComponents;
+
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HandyBoxApp.UserControls
+{
+    public class TitlePanel : UserControl
+    {
+        //################################################################################
+        #region Fields
+
+        private Label logoLabel;
+        private Label titleLabel;
+        private Label closeLabel;
+        private FlowLayoutPanel containerPanel;
+
+        #endregion
+
+        //################################################################################
+        #region Constructor
+
+        public TitlePanel()
+        {
+            InitializeComponent();
+            ReorderComponents();
+        }
+
+        #endregion
+
+        //################################################################################
+        #region Private Members
+
+        private void InitializeComponent()
+        {
+            containerPanel = new FlowLayoutPanel();
+            logoLabel = new Label();
+            titleLabel = new Label();
+            closeLabel = new Label();
+
+            containerPanel.SuspendLayout();
+            SuspendLayout();
+
+            // 
+            // ContainerPanel
+            // 
+            containerPanel.Margin = new Padding(0);
+            containerPanel.Padding = new Padding(0);
+            containerPanel.Location = new Point(0, 0);
+            containerPanel.FlowDirection = FlowDirection.LeftToRight;
+
+            containerPanel.Controls.Add(logoLabel);
+            containerPanel.Controls.Add(titleLabel);
+            containerPanel.Controls.Add(closeLabel);
+
+            // 
+            // LogoLabel
+            // 
+            logoLabel.Name = "LogoLabel";
+            logoLabel.Text = "L";
+            logoLabel.AutoSize = true;
+            logoLabel.Margin = new Padding(0, 0, Style.PanelSpacing, 0);
+            logoLabel.Padding = new Padding(Style.PanelPadding);
+            logoLabel.TextAlign = ContentAlignment.MiddleCenter;
+            logoLabel.Font = new Font(new FontFamily(Style.FontName), Style.PanelFontSize, FontStyle.Bold);
+            Painter<Green>.Paint(logoLabel, PaintMode.Light);
+
+            // 
+            // TitleLabel
+            // 
+            titleLabel.Name = "TitleLabel";
+            titleLabel.Text = "Handy Box App";
+            titleLabel.AutoSize = true;
+            titleLabel.Margin = new Padding(0, 0, Style.PanelSpacing, 0);
+            titleLabel.Padding = new Padding(Style.PanelPadding);
+            titleLabel.TextAlign = ContentAlignment.MiddleLeft;
+            titleLabel.Font = new Font(new FontFamily(Style.FontName), Style.PanelFontSize, FontStyle.Bold);
+            Painter<Black>.Paint(titleLabel, PaintMode.Dark);
+
+            // 
+            // CloseLabel
+            // 
+            closeLabel.Name = "CloseLabel";
+            closeLabel.Text = "X";
+            closeLabel.AutoSize = true;
+            closeLabel.Margin = new Padding(0, 0, 0, 0);
+            closeLabel.Padding = new Padding(Style.PanelPadding);
+            closeLabel.TextAlign = ContentAlignment.MiddleCenter;
+            closeLabel.Font = new Font(new FontFamily(Style.FontName), Style.PanelFontSize, FontStyle.Bold);
+            Painter<Red>.Paint(closeLabel, PaintMode.Dark);
+
+            // 
+            // TitlePanel
+            // 
+            Name = "TitlePanel";
+            AutoSize = true;
+            Margin = new Padding(0, 0, 0, Style.PanelSpacing);
+            BackColor = Color.FromArgb(100, 100, 100);
+            BorderStyle = BorderStyle.FixedSingle;
+            AutoScaleMode = AutoScaleMode.Font;
+
+            Controls.Add(containerPanel);
+
+            containerPanel.ResumeLayout(false);
+            containerPanel.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        private void ReorderComponents()
+        {
+            containerPanel.Width = 0;
+            containerPanel.Height = 0;
+
+            foreach (Control control in containerPanel.Controls)
+            {
+                if (control.PreferredSize.Height > containerPanel.Height)
+                {
+                    containerPanel.Height = control.PreferredSize.Height;
+                }
+
+                containerPanel.Width += control.PreferredSize.Width;
+            }
+
+            containerPanel.Width += containerPanel.Controls.Count - 1;
+            Height = containerPanel.Height;
+            Width = containerPanel.Width;
+        }
+
+        #endregion
+    }
+}
