f0c3cd8015e8d053aed1375c29ade3148013edd6
halityurtsever <halityurtsever@gmail.com>
2019-11-17 18:58:14 +0100

    Stock factory and stock list xml are added

---
+++ HandyBoxApp.StockExchange/StockServiceFactory.cs
@@ -0,0 +1,34 @@
+﻿using HandyBoxApp.StockExchange.Interfaces;
+using HandyBoxApp.StockExchange.StockService;
+
+using System;
+
+namespace HandyBoxApp.StockExchange
+{
+    public class StockServiceFactory
+    {
+        //################################################################################
+        #region Constructor
+
+        private StockServiceFactory() { }
+
+        #endregion
+
+        //################################################################################
+        #region Public Static Members
+
+        public static IStockService CreateService(string serviceName)
+        {
+            switch (serviceName)
+            {
+                case "Yahoo":
+                    return new YahooStockService();
+
+                default:
+                    throw new ArgumentException("Invalid stock serviceType argument.");
+            }
+        }
+
+        #endregion
+    }
+}
