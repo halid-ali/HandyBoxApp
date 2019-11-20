64b4867f261c8bcac2c1991a7b7ac8a786e652ac
halityurtsever <halityurtsever@gmail.com>
2019-11-20 17:35:48 +0100

    overtime argument is added to timer update event args

--- HandyBoxApp/WorkTimer/TimerUpdateEventArgs.cs
+++ HandyBoxApp/WorkTimer/TimerUpdateEventArgs.cs
@@ -4,14 +4,17 @@ namespace HandyBoxApp.WorkTimer
 {
     internal class TimerUpdateEventArgs
     {
-        public TimerUpdateEventArgs(TimeSpan elapsedTime, TimeSpan remainingTime)
+        public TimerUpdateEventArgs(TimeSpan elapsedTime, TimeSpan remainingTime, TimeSpan overtime)
         {
             ElapsedTime = elapsedTime;
             RemainingTime = remainingTime;
+            Overtime = overtime;
         }
 
         internal TimeSpan ElapsedTime { get; }
 
         internal TimeSpan RemainingTime { get; }
+
+        internal TimeSpan Overtime { get; }
     }
 }
